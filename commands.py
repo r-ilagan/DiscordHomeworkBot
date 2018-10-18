a98eb7b12cf2ef0b352b23dfbbf6103ba1b04a56
Rach Ilagan <cachola.matthew@gmail.com>
2018-10-17 23:19:30 -0400

    Added help command for bot

---
+++ commands.py
@@ -0,0 +1,12 @@
+
+def print_help():
+    help=("Hi there!\n" 
+          "So I can help you properly, please use the following commands:\n"
+          "```"
+          "--!help\n "
+          "      -> for more information\n"
+          "--!ass [assignment name/course code] [YYYY/MM/DD (Due date)] \n"
+          "      -> to add new assignment\n"
+          "--!test  [Course code] [YYYY/MM/DD (Due date)] \n"
+          "      -> to add new test or exam```")
+    return help
--- homework-bot.py
+++ homework-bot.py
@@ -1,10 +1,23 @@
+# version 0.1 of homework-bot
 
 import discord
-
+import commands
 
 client = discord.Client()
 token = open("token.txt", "r").read()
 
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user} ")
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+
+    # prints commands
+    if "!help" == message.content:
+        await channel.send(commands.print_help())
 
 
 
